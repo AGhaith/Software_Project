8e1554f0e15d33debfe3d68b89eec9ef97abe100
Salma <S.ali2369@nu.edu .eg>
2025-12-13 18:35:26 +0200

    rebased

---
+++ Backend/LocalBrandFinder.Application/CreateProductDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalBrandFinder.Application
+{
+    public class CreateProductDTO
+    {
+        public required string Name { get; set; }
+        public required string Description { get; set; }
+        public required string Price { get; set; }
+        public required string Type { get; set; }
+        public required List<string> AvailableSizes { get; set; }
+        public required int AvailableStock { get; set; }
+    }
+}
