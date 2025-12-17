6bd14ac4a9c2ca6cdaf5c0c361a6c480b25e68b9
jannahashraf <jannahashraf10@gmail.com>
2025-12-17 19:56:00 +0200

    Added brand dashboard, details, profile, and category management

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -3,7 +3,11 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { SignIn } from "./pages/SignIn";
 import { SignUp } from "./pages/SignUp";
+import BrandDashboard from "./pages/BrandDashboard";
+import CategoryManagement from "./pages/CategoryManagement";
+import BrandProfile from "./pages/BrandProfile";
 import "./App.css";
+import BrandDetail from "./pages/BrandDetail";
 
 function App() {
   return (
@@ -12,6 +16,10 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/brand_dashboard" element={<BrandDashboard />} />
+        <Route path="/brand_profile" element={<BrandProfile />} />
+        <Route path="/category_management" element={<CategoryManagement />} />
+        <Route path="/brand_detail" element={<BrandDetail />} />
       </Routes>
     </Router>
   );
