51683bfa9596334025c32c39a98ccc8e5ada681a
Ahmed Ghaith <a.wesam2300@nu.edu.eg>
2025-12-17 21:23:38 +0200

    fixed the endpoint add product to brand and support for sending multiple images and ran appropriate migrations

--- Backend/LocalBrandFinder.API/Controllers/BrandController.cs
+++ Backend/LocalBrandFinder.API/Controllers/BrandController.cs
@@ -121,7 +121,7 @@ namespace LocalBrandFinder.API.Controllers;
     }
     [HttpPatch("add/{brandId}/Product/{ProductId}")]
     [Authorize(Roles = "Brand")]
-    public async Task<IActionResult> AddProductToBrand(Guid brandId, CreateProductDTO productdto)
+    public async Task<IActionResult> AddProductToBrand(Guid brandId, Guid ProductId)
     {
         
         var brandList = await _unitOfWork.Brands.GetAsync(
