0aa1e4a3374c6d9819ff8a026724b9a92b068830
Salma <S.ali2369@nu.edu .eg>
2025-12-21 02:18:55 +0200

    Edited the DTO name and added description

--- Backend/LocalBrandFinder.Application/DTOs/Authentication/CreateCategoryDTO.cs
+++ Backend/LocalBrandFinder.Application/DTOs/Authentication/CreateCategoryDTO.cs
@@ -6,8 +6,9 @@ using System.Threading.Tasks;
 
 namespace LocalBrandFinder.Application.DTOs.Authentication
 {
-    internal class CategoryCreateDto
+    internal class CreateCategoryDTO
     {
         public string Name { get; set; } = string.Empty;
+        public string Description { get; set; }
     }
 }
