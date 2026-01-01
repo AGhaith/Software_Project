3f79312062863ac961273f4653f93ded39f66035
Ahmed Ghaith <a.wesam2300@nu.edu.eg>
2026-01-01 17:02:24 +0200

    cm

---
+++ Backend/LocalBrandFinder.Application/DTOs/Authentication/CreateCategoryDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace LocalBrandFinder.Application.DTOs.Authentication
+{
+    internal class CreateCategoryDTO
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public string? Description { get; set; }
+    }
+}
